ddfadb29d94577dd45f2c4997c73fff0d32be2c9
Andrew_Moryakov <topazd2@gmail.com>
2015-07-28 01:01:44 +0300

    push

---
+++ ServerStikerSalle/ServerStikerSalle/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ServerStikerSalle.Startup))]
+namespace ServerStikerSalle
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
